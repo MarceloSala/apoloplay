6ff2feec62f89b8b9eaed7697d727a38c51d74af
Danielvilla2 <danieduardovilla30@gmial.com>
2022-07-06 19:32:44 -0500

    Agregado el login

---
+++ apps/auth/src/app/Components/Login/componente.tsx
@@ -0,0 +1,13 @@
+import './estilogin.css';
+
+export const login = () => {
+    return (
+        <section className="forma-login">
+        <h1>¡Bienvenido!</h1>
+        <input className="cajatexto" type="email" name ="correo" id="correo" placeholder="Correo" />
+        <input className="cajatexto" type="password" name ="contraseña" id="contraseña" placeholder="Contraseña" />
+        <input className="boton_login" type="submit" value="Ingresar"/>
+        <p>¿No tienes cuenta? <a href="#">Registrate</a></p>
+    </section>
+    )
+}
