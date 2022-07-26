ba5a5cbfbfb5c99ed8f6c2efdaaebd5ebc2bb9ba
MarceloSalaA <hector.sala@enroutesystems.com>
2022-07-26 13:41:29 -0500

    Added Dockerfile to each app

--- apps/music-service/src/main.ts
+++ apps/music-service/src/main.ts
@@ -4,14 +4,6 @@ import router from './app/routes';
 
 const app = express();
 
-//middlewares
-app.use((res, req, next) => {
-  next();
-});
-
-//routes
-app.use(router);
-
 const port = process.env.port || 3001;
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port);
